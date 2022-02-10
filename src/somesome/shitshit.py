2bbafd325bfb201269186981d4f43e1685dd25b8
NayaxEmbeddedAutomation <49714264+NayaxEmbeddedAutomation@users.noreply.github.com>
2022-02-10 18:55:45 +0200

    Add existing file

---
+++ src/somesome/shitshit.py
@@ -0,0 +1,11 @@
+# def print_hi(name):
+#     return f'Hi, {name}'
+
+
+def print_equal(x, y):
+    if not isinstance(x, int):
+        return False
+    if not isinstance(y, int):
+        return False
+
+    return True if x == y else False
---
+++ tests/test_some_shit.py
@@ -0,0 +1,14 @@
+from somesome.shitshit import print_equal
+import pytest
+
+
+@pytest.mark.parametrize("a, b, c", [
+    (1, 3, False),
+    (3, 3, True),
+    ("a", 3, False),
+    (3, "a", False),
+
+])
+
+def test_some_shit(a, b, c):
+    assert print_equal(a, b) == c
